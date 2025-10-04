c5ba2f0812d33f21e9711d235b12905cd59cddf0
Hazrat90 <127823732+Hazrat90@users.noreply.github.com>
2025-10-03 18:33:03 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+content: ["./src/**/*.{js,ts,jsx,tsx}"],
+theme: {
+extend: {},
+},
+plugins: [],
+};
+
+
+// src/styles/globals.css
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+
+body {
+font-family: 'Inter', sans-serif;
+}
